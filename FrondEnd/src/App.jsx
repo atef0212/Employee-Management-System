ec5ac5c6b10623eeca753e23261bff5c81a19e18
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-04-30 10:06:26 +0200

    navBar in prosses

--- FrondEnd/src/App.jsx
+++ FrondEnd/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout'
 import LogIn from './login'
 
 function App() {
 
   return (
     <>
-<LogIn/>
+
+<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Layout/>}/>
+    <Route path='/login' element={<LogIn/>}/>
+  </Routes>
+</BrowserRouter>
     </>
   )
 }
