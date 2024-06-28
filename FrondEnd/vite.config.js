2b4d52a168e0c1618f4a201bc93d5dafa8a0d02f
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-06-28 12:56:13 +0200

    .

--- FrondEnd/vite.config.js
+++ FrondEnd/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-service.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
