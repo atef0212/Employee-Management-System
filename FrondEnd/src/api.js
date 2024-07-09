fd9dfca5d440a0c6eb6755557d7cf4eeb5a3df46
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-07-09 11:00:43 +0200

    .

--- FrondEnd/src/api.js
+++ FrondEnd/src/api.js
@@ -1,3 +1,3 @@
 const isLocal = true; // or false, depending on your condition
-const url_Api = isLocal ? "http://localhost:5000/api/" : "https://employee-management-s.onrender.com";
+const url_Api = isLocal ? "http://localhost:5000/api/" : "https://employee-management-system-git-main-atefs-projects-7dae53ef.vercel.app/";
 export default url_Api
